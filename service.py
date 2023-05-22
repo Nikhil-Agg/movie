7876b072aae1021c9d4fc1b55a2eb05d7b4f6908
Nikhil-Agg <nikhigrwl40@gmail.com>
2023-05-22 21:34:38 +0530

    fix: service.py

--- service.py
+++ service.py
@@ -9,13 +9,13 @@ logger = logging.getLogger()
 
 app = webapp2.WSGIApplication(MOVIE_ROUTES, debug=True)
 
-def main():
-    try:
-        Initialize.initialize_service_startup_dependencies()
-        # httpserver.serve(app, host='127.0.0.1', port='8080')
-        app.run()
-    except Exception as e:
-        logger.exception("Something went wrong while starting service :: error - {}".format(e))
+# def main():
+#     try:
+#         Initialize.initialize_service_startup_dependencies()
+#         # httpserver.serve(app, host='127.0.0.1', port='8080')
+#         app.run()
+#     except Exception as e:
+#         logger.exception("Something went wrong while starting service :: error - {}".format(e))
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
