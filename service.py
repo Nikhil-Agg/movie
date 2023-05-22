84829bb76a23fa56de94ce031c2d36652643b882
Nikhil-Agg <nikhigrwl40@gmail.com>
2023-05-22 21:04:09 +0530

    feat: logging for service startups

--- service.py
+++ service.py
@@ -4,12 +4,18 @@ from paste import httpserver
 from initialize import Initialize
 from routes.movie_routes import MOVIE_ROUTES
 
+import logging
+logger = logging.getLogger()
+
 app = webapp2.WSGIApplication(MOVIE_ROUTES, debug=True)
 
 def main():
-    Initialize.initialize_service_startup_dependencies()
-    # httpserver.serve(app, host='127.0.0.1', port='8080')
-    app.run()
+    try:
+        Initialize.initialize_service_startup_dependencies()
+        # httpserver.serve(app, host='127.0.0.1', port='8080')
+        app.run()
+    except Exception as e:
+        logger.exception("Something went wrong while starting service :: error - {}".format(e))
 
 if __name__ == '__main__':
     main()
